7e08a2b7b2b786602e19ab6af40a863d33e1e088
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-08-04 01:43:44 +0300

    SolarCoin, ChronoBank and Quanta service dependencies removed

--- src/Lykke.Job.TransactionHandler.Core/Services/Quanta/IQuantaService.cs
+++ src/Lykke.Job.TransactionHandler.Core/Services/Quanta/IQuantaService.cs
@@ -1,12 +1,9 @@
 ﻿using System.Threading.Tasks;
-using Lykke.Job.TransactionHandler.Core.Domain.BitCoin;
 
 namespace Lykke.Job.TransactionHandler.Core.Services.Quanta
 {
     public interface IQuantaService
     {
-        Task<string> SetNewQuantaContract(IWalletCredentials walletCredentials);
         Task SendCashOutRequest(string id, string addressTo, double amount);
-        Task<bool> IsQuantaUser(string address);
     }
 }
